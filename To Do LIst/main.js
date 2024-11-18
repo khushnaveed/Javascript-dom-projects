11c64bbec51f69c07d6aa552790688dceb140a00
Khush Bakht Naveed <khush.naveed@dci-student.org>
2024-11-18 15:31:23 +0100

    add task funcationality

--- To Do LIst/main.js
+++ To Do LIst/main.js
@@ -1,14 +1,25 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const div = document.querySelector('.task-list');
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const div = document.querySelector(".task-list");
 
 
-
-const task = document.createElement('div');
-task.className = 'task';
-
-const ul = document.createElement('ul');
-const li = document.createElement('li');
-
-const xIcon = document.createElement('span')
-xIcon.innerHTML = 'x'
+button.addEventListener('click', (e) =>{
+  e.preventDefault();
+  if(input.value !== ''){
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    const xIcon = document.createElement("span");
+  
+    li.textContent = input.value;
+    
+    xIcon.textContent = "x";
+    li.append(xIcon);
+    ul.append(li);
+    div.append(ul);
+  
+    input.value = "";
+  }else{
+    input.style.border = "2px solid red"
+  }
+ 
+});
