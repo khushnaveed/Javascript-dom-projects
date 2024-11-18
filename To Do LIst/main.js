963c48522516d69eba08878b6cdcacd8f3293ce4
Khush Bakht Naveed <khush.naveed@dci-student.org>
2024-11-18 15:11:12 +0100

    add html code

---
+++ To Do LIst/main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const div = document.querySelector('.task-list');
+
+
+
+const task = document.createElement('div');
+task.className = 'task';
+
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+
+const xIcon = document.createElement('span')
+xIcon.innerHTML = 'x'
